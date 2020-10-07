4e94459acf78265dc538cb87efd05eccb93071b2
Antony <antony.jacques@laposte.net>
2020-10-07 14:01:52 +0200

    Update article-preview.tpl.php

--- templates/article-preview.tpl.php
+++ templates/article-preview.tpl.php
@@ -11,8 +11,32 @@
             </form>
             
         </div>
-        <div class="card-footer"><a href="/category/sports"><span
-                    class="badge badge-secondary"><?= $article['category'] ?></span></a> <small class="text-muted">Published on
+        <div class="card-footer">
+        <!--
+        <form action="category.php">
+            <input type="hidden" name="categoryQuery" value="<?= $article['category'] ?>" >
+            <button type="submit" class="btn btn-outline-success">test</button>
+
+        </form>
+
+
+
+
+
+        -->
+    <a href="/category.php?category=<?= $article['category'] ?>">
+         <span
+            class="badge badge-secondary"><?= $article['category'] ?>
+        </span>
+    </a> 
+    
+    <small class="text-muted">Published on
                 <?= $article['date'] ?></small></div>
     </div>
 </li>
+<!--
+Votre input fonctionnera seulement si il se trouve dans une balise form
+Donc soit vous avez une balise a avec un lien qui contient "mapage.php?cle=valeur"
+Soit vous avez une balise form avec action="mapage.php" et un input en type hidden qui contient name="cle" et value="valeur"
+
+-->
